b016cbc5660327a6551478e129b2cd2c10c3d652
taiyo <moegi822@gmail.com>
2026-05-02 14:08:47 +0900

    結果画面を作成

---
+++ jackhack2026-temg/src/finish.tsx
@@ -0,0 +1,56 @@
+const correctcount = 0
+
+function CorrectCount() {
+    return (
+        <div className = 'correctcount'>
+            { correctcount } 問
+            <span className = 'correctcount-correct'>
+                正解
+            </span>
+            ！
+        </div>
+    )
+}
+
+function FinishComment() {
+    const finishcomment = 'よくがんばったね！すご～い！！'
+
+    return (
+        <div className = 'finishcomment'>
+            <p>{ finishcomment }</p>
+            <img />
+        </div>
+    )
+}
+
+function FinishCommentImg() {
+    return (
+        <div className = 'finishcommentimg'>
+            <img src='' />
+        </div>
+    )
+}
+
+function BackToTitle() {
+    return (
+        <div className = 'backtotitle'>
+            <button>
+                タイトルにもどる
+            </button>
+        </div>
+    )
+}
+
+export default function Finish() {
+    return (
+        <div className = 'result'>
+            <div className = 'result-top'>
+                結果
+            </div>
+            <CorrectCount />
+            <FinishComment />
+            <FinishCommentImg />
+            <BackToTitle />
+        </div>
+    )
+}
--- jackhack2026-temg/src/main.tsx
+++ jackhack2026-temg/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Finish from './finish';
+import './finish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Finish />
   </StrictMode>,
 )
