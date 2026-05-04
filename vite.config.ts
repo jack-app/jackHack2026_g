811a196fa3c929ae2e4f6c8c836f8f3164bb10d7
mn0803 <watervalley83@gmail.com>
2026-05-04 13:04:47 +0900

    タイポ修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/jackhack2026_g/",
+  base: "/jackHack2026_g/",
   plugins: [react()],
 });
